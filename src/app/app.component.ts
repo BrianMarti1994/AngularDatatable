6c55c3eef3715b4d7fe7cc8cf39faeb63e18063a
Brian <brianmarti1994@gmail.com>
2020-10-17 17:52:13 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
-
+import { Component ,OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import{HttpClient,HttpHeaders } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AngularDatatable';
+export class AppComponent implements OnDestroy, OnInit {
+
+// We use this trigger because fetching the list of employee can be quite long,
+  // thus we ensure the data is fetched before rendering
+  dtTrigger: Subject<any> = new Subject();
+  dtOptions: DataTables.Settings = {};
+  employee :object;
+constructor(private http: HttpClient) { }
+  ngOnInit(): void {
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 2
+    };
+    this.getEmployeeData()
+  }
+  
+
+getEmployeeData()
+{
+  this.http.get('assets/Employee.json').subscribe(data => {
+    this.employee =data
+  
+    // Calling the DT trigger to manually render the table
+    this.dtTrigger.next();
+  });
+  
 }
+ngOnDestroy(): void {
+  this.dtTrigger.unsubscribe();
+}
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import{HttpClientModule} from '@angular/common/http';
+import { DataTablesModule } from 'angular-datatables';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    DataTablesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
